c1665c1b8816e9573a6f149c51bb1818d7925b95
chrillah <ch.ronnberg@gmail.com>
2023-04-05 20:13:53 +0200

    login module updated

--- DAAJE/src/components/LogInModule.vue
+++ DAAJE/src/components/LogInModule.vue
@@ -1,32 +1,93 @@
 <script>
 export default {
-    data(){
-        return{
-            emailInput : '',
-            passwordInput : ''
-        }
+  data() {
+    return {
+      logIn: true,
+      email: '',
+      password: '',
+      userName : '',
+      signEmail : '',
+      signPassword : ''
+    };
+  },
+  methods: {
+    handleSignIn(){
+        console.log(`Användarnamn ${this.userName}Epost ${this.signEmail}, lösenord ${this.signPassword}`);
+    },
+    handleLogIn() {
+      console.log(`Epost ${this.email}, lösenord ${password}`);
+    },
+    signingIn() {
+        this.logIn = !this.logIn
+    },
+    forgotPassword(){
+        console.log('ok')
     }
+  },
 };
 </script>
 
 <template>
   <div class="login-container">
     <div class="btn-container">
-      <button @click="toggleShow" class="close-module-container">X</button>
-      <button class="close-module-container">Tillbaka</button>
+      <button v-show="logIn" @click="toggleShow" class="close-module-container">X</button>
+      <button v-show="!logIn" @click="signingIn" class="close-module-container">Tillbaka</button>
     </div>
-    <form class="login-regular-container">
+
+    <form v-show="logIn" @submit.prevent="handleLogIn" class="login-regular-container">
+      <div class="input-container">
         <label class="input-label">Epost</label>
-        <input class="input-value" v-model="emailInput" type="email" required>
+        <input class="input-value" v-model="email" type="email" required />
+      </div>
+      <div class="input-container">
         <label class="input-label">Lösenord</label>
-        <input class="input-value" v-model="passwordInput" type="password" required>
+        <input
+          class="input-value"
+          v-model="password"
+          type="password"
+          required
+        />
+      </div>
+      <div class="submit-btn-container">
+        <button type="submit" class="login-btn">Logga in</button>
+        <p>Eller</p>
+        <button @click="signingIn" class="signIn-btn">Skapa konto</button>
+        <button @click="forgotPassword" class="forgot-btn">Glömt lösenord?</button>
+      </div>
     </form>
-    <h1>LOGIN</h1>
+
+    <form v-show="!logIn" @submit.prevent="handleSignIn" class="login-new-container">
+        <div class="input-container">
+        <label class="input-label">Användarnamn</label>
+        <input
+          class="input-value"
+          v-model="userName"
+          type="text"
+          required
+        />
+      </div>
+      <div class="input-container">
+        <label class="input-label">Lösenord</label>
+        <input
+          class="input-value"
+          v-model="signPassword"
+          type="password"
+          required
+        />
+      </div>
+      <div class="input-container">
+        <label class="input-label">Epost</label>
+        <input class="input-value" v-model="signEmail" type="email" required />
+      </div>
+      <div class="submit-btn-container">
+        <button type="submit" class="login-btn">Skapa konto</button>
+      </div>
+    </form>
   </div>
 </template>
 
 <style>
 .login-container {
-  background-color: blue;
+  background-color: white;
 }
 </style>
