7d755e9dcd88a26f3aa88405a1e0fe7dfb4779e2
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-04-11 10:39:30 +0200

    add conditional rendering of whyCorrect & styling

--- DAAJE/src/components/TheResults.vue
+++ DAAJE/src/components/TheResults.vue
@@ -5,10 +5,10 @@ import { useResultStore } from '../stores/resultStore';
 const ShadowPlugin = {
   beforeDraw: (chart) => {
     const { ctx } = chart;
-    ctx.shadowColor = "rgba(0, 0, 0, 0.5)";
+    ctx.shadowColor = 'rgba(0, 0, 0, 0.5)';
     ctx.shadowBlur = 10;
-    ctx.shadowOffsetX = -10;
-    ctx.shadowOffsetY = -5;
+    ctx.shadowOffsetX = 10;
+    ctx.shadowOffsetY = 5;
   },
 };
 export default {
@@ -28,30 +28,19 @@ export default {
 
     return { question, answers, results };
   },
-  data() {
-    return {
-      welcomeMessage: '',
-      fetchedResultData: [],
-    };
-  },
-  methods: {},
-  computed: {
-    studentAnsweredWrong() {
-      // skapa funktion för att beräkna om användaren svarade fel,
-      // så ska förklaring till svaret visas.
-      return "not done";
-    },
-  },
   mounted() {
     const ctx = document.getElementById('myChart');
     const MyChart = new Chart(ctx, {
-      type: "pie",
+      type: 'pie',
       data: {
-        labels: ["Rätt svar", "Fel svar"],
+        labels: ['Rätt svar', 'Fel svar'],
         datasets: [
           {
-            label: "Resultat av Quiz",
-            data: [this.sumOfCorrectAnswers, (this.quizLength - this.sumOfCorrectAnswers)],
+            label: 'Resultat av Quiz',
+            data: [
+              this.sumOfCorrectAnswers,
+              this.quizLength - this.sumOfCorrectAnswers,
+            ],
             // backgroundColor: "rgba(54,73,93,.5)",
             // borderColor: "#36495d",
             borderWidth: 3,
@@ -79,74 +68,76 @@ export default {
 </script>
 
 <template>
-<div class="white-bg">
+  <div class="white-bg">
     <div class="container">
-        <div class="row">
-            <div class="col-lg-8">
-                <h3>Resultat av ditt quiz!</h3>
-                <div class="chart-container">
-                    <canvas id="myChart"></canvas>
-                </div>
-                <h3 class="mb-3">Enskilda resultat av frågor:</h3>
-                <div v-for="(result, index) in results" :key="index">
-                    <h5>Fråga {{ index + 1 }}, {{ result.question.text }}</h5>
-                    <p>
-                        Rätt svar:
-                        {{
-                            result.question.options.filter(
-                                (option) => option.isCorrect === true
-                            )[0].label
-                        }},
-                        {{
-                            result.question.options.filter(
-                                (option) => option.isCorrect === true
-                            )[0].text
-                        }}
-                    </p>
-                    <p class="pb-2"  :style="{
-                        color: result.option.isCorrect
-                            ? '#198754'
-                            : '#dc3545'
-                    }">
-                        Du valde: {{ result.option.label }}.
-                        {{ result.option.text }}
-                    </p>
-                    <p class="pb-3" v-if="studentAnsweredWrong">
-                        Förklaring till rätt svar...
-                        {{
-                            result.question.options.filter(
-                                (option) => option.isCorrect === true
-                            )[0].whyCorrect
-                        }}
-                    </p>
-                </div>
+      <div class="row">
+        <div class="col-lg-8">
+          <h3>Resultat av ditt quiz!</h3>
+          <div class="chart-container">
+            <canvas id="myChart"></canvas>
+          </div>
+          <h3 class="mb-4">Enskilda resultat av frågor:</h3>
+          <div v-for="(result, index) in results" :key="index" class="mb-4">
+            <h5>Fråga {{ index + 1 }}, {{ result.question.text }}</h5>
+            <p>
+              Rätt svar:
+              {{
+                result.question.options.filter(
+                  (option) => option.isCorrect === true
+                )[0].label
+              }},
+              {{
+                result.question.options.filter(
+                  (option) => option.isCorrect === true
+                )[0].text
+              }}
+            </p>
+            <p
+              class="pb-2"
+              :style="{
+                color: result.option.isCorrect ? '#198754' : '#dc3545',
+              }"
+            >
+              Du valde: {{ result.option.label }}.
+              {{ result.option.text }}
+            </p>
+            <div class="pb-3" v-if="!result.option.isCorrect">
+              <p>Anledning till rätt svar,</p>
+              <p>
+                {{
+                  result.question.options.filter(
+                    (option) => option.isCorrect === true
+                  )[0].whyCorrect
+                }}
+              </p>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 <style scoped>
 h3 {
-    text-align: center;
+  text-align: center;
 }
 
 .container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 3rem;
-    overflow-x: hidden;
-    margin-top: auto;
-    background-color: rgba(255, 255, 255, 0.844);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 3rem;
+  overflow-x: hidden;
+  margin-top: auto;
+  background-color: rgba(255, 255, 255, 0.844);
 }
 
 .row {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    overflow: visible;
-    padding: 1rem 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow: visible;
+  padding: 1rem 0;
 }
-
 </style>
