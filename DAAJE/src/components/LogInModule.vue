47ffec46bef69e2827cb31aca2f26f4e9b9f73d3
chrillah <ch.ronnberg@gmail.com>
2023-04-14 10:32:24 +0200

    useravatar shows login status

--- DAAJE/src/components/LogInModule.vue
+++ DAAJE/src/components/LogInModule.vue
@@ -24,9 +24,12 @@ export default {
     showLogin(boolean) {
       this.$emit('booleanToParent', boolean);
     },
+    userIsLoggedIn(status) {
+        this.$emit('loggedIn', status)
+    },
     async handleSignIn(){
           console.log(
-        `Användarnamn ${this.userName}Epost ${this.signEmail}, lösenord ${this.signPassword}`
+        `Användarnamn ${this.userName} Epost ${this.signEmail}, lösenord ${this.signPassword}`
       );
         await axios.post("http://localhost:8080/post/new_user", {
             username: this.userName,
@@ -39,17 +42,19 @@ export default {
 
     },
     async handleLogIn() {
-      const usertoken = await axios.post("http://localhost:8080/post/login", 
+      const usertoken = await axios.post("http://localhost:8080/post/login",
           {
             email: this.email,
             password: this.password
           }
       );
-      if(usertoken) { 
+      if(usertoken) {
         localStorage.setItem('usertoken', usertoken.data); // store this in pinia instead? @johan
         this.$loggedInStatus = true; // this bool is for tracking logged in status
         // call methods here to update GUI with user details @chris
-      } 
+
+        this.userIsLoggedIn(this.$loggedInStatus)
+      }
       this.showLogin(false);
     },
     signingIn() { /* byt vy mellan signin/login */
@@ -204,19 +209,8 @@ export default {
   bottom: 0.3rem;
 }
 .login-wrapper {
-  /* --black: #000000;
-  --white: #ffffff;
-  --purple: #5f0a87;
-  --light-purple: #D3C8E4;
-  --error: #A81621; */
   display: grid;
   grid-template-columns: 5px auto 5px;
-  /* position: fixed;
-  z-index: 9999;
-  top: 0.2rem;
-  left: 0.3rem;
-  right: 0.3rem;
-  bottom: 0.3rem; */
 }
 
 .clickable-area{
@@ -230,11 +224,6 @@ export default {
   background-color: var(--frosty-purple-bg);
   backdrop-filter: blur(20px);
   border-radius: 10px;
-
-  /* olika färger till bakgrundet */
-  /* background-color: rgba(244, 244, 244, 0.65); */
-  /* background-color: rgba(95, 10, 135, 0.5); */
-  /* overflow: scroll; */
 }
 
 @media (min-width: 400px) {
@@ -368,11 +357,6 @@ button {
 .signIn-btn {
   color: var(--purple);
   border: 3px solid var(--purple);
-
-  /* ett försök till gradient stroke */
-  /* border-style: solid;
-  border-width: 3px; */
-  /* border-image: linear-gradient(306deg, #c164ec 0%, #5f0a87 84%) 1; */
 }
 
 .forgot-btn-container {
