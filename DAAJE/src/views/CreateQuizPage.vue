37d91d5a67dcee831c8da97c8679a82b1dab8191
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-04-18 11:26:17 +0200

    remove userAvatar

--- DAAJE/src/views/CreateQuizPage.vue
+++ DAAJE/src/views/CreateQuizPage.vue
@@ -1,11 +1,9 @@
 <script setup>
 import QuizForm from "../components/QuizForm.vue";
-// import UserAvatar from "../components/UserAvatar.vue";
 </script>
 
 <template>
   <div class="pt-medium">
-    <!-- <UserAvatar /> -->
     <div class="hero-text">
       <h1>Skapa ditt egna quiz!</h1>
       <h4>Skriv frågor och lägg till svarsalternativ</h4>
