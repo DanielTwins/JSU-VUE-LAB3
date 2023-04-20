b7fbf44f61552c51be2786a6d42bd7efe8bdf9fb
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-04-20 14:05:58 +0200

    change back to quizcard

--- DAAJE/src/components/QuizQuestions.vue
+++ DAAJE/src/components/QuizQuestions.vue
@@ -2,7 +2,7 @@
   <section class="d-flex">
     <!-- <button style="border: 1px solid black">Knapp till Result</button> -->
     <!-- <QuizCard v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" /> -->
-    <ResultCard
+    <QuizCard
     v-for="quiz in quizes"
     :key="quiz.id"
     :quiz="quiz"
@@ -16,17 +16,16 @@ import axios from "axios";
 
 // import QuizCard from "./QuizCard.vue";
 import { ref } from "vue";
-import ResultCard from "./ResultCard.vue";
-
+import QuizCard from "./QuizCard.vue";
 
 export default {
-  components: { ResultCard },
+  components: { QuizCard },
   async setup() {
     const userid = localStorage.getItem("usertoken");
     const result = await axios.get((userid? `http://localhost:8080/quiz_questions/${userid}` : "http://localhost:8080/quiz_questions"));
     const quizes = ref(result.data);
-    const ouid = "6438362cf7eacfc8b8a276d1";//sample
-    const quizId = "643872d185dd4fbabfa27d8b";//sample
+    const ouid = "6438362cf7eacfc8b8a276d1";// sample
+    const quizId = "643872d185dd4fbabfa27d8b";// sample
     // add new list of custom quizzes from another user
     const getCustomQuizFromElis = await axios.get((`http://localhost:8080/shared_quiz_questions/${ouid}/${quizId}`));
 
